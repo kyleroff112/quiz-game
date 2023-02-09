a44f0a7d8f496bcac34f37e9b29f33d29e4f6827
Kyle Roff <113395568+kyleroff112@users.noreply.github.com>
2023-02-09 11:41:20 -0500

    Feature branch (#2)
    * added code to everything pretty much
    
    * added a whole lotta code, general functionality

--- quiz.js
+++ quiz.js
@@ -1,162 +1,95 @@
-var quizQuestions = ["Who created Javascript?", "What is first browser that supported Javascript?", "What does console logging the window do?", "What is the correct syntax for a function?", "What year was Javascript created?"];
-var quizAnswers = ["Brendan Eich", "Netscape", "Opens the console", "function myFunction()", "1995"];
+var questions = [
+  {
+    question: "Who created JavaScript?",
+    choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich", "Bill Gates"],
+    answer: "Brendan Eich"
+  },
+  {
+    question: "When was JavaScript created?",
+    choices: ["1995", "1996", "1994", "none of the above"],
+    answer: "1995"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    choices: ["Node.js", "TypeScript", "npm", "All of the above"],
+    answer: "npm"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    choices: ["Angular", "jQuery", "RequireJS", "ESLint"],
+    answer: "ESLint"
+  },
+  {
+    question: "Which of the following is not a JavaScript framework?",
+    choices: ["Angular", "React", "Vue", "jQuery"],
+    answer: "jQuery"
+  },
+];
+
+console.log("Hello World");
+
+var startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  document.getElementById('question-section').style.display = 'block';
+  nextQuestion();
+  // var intervalId = setInterval(decrementTime, 1000);
+  var intervalId = setInterval(function () {
+    seconds--;
+    document.getElementById("timer").innerHTML = seconds + " seconds remaining";
+    if (seconds === 0) {
+      clearInterval(intervalId);
+      endGame();
+      alert("Time's up! The quiz is over.");
+    }
+  }, 1000);
+});
+
+var index = 0;
+var score = 0;
+
+// Set the number of seconds for the timer
+var seconds = 60;
+
+// Display the starting time on the page
+document.getElementById("timer").innerHTML = seconds + " seconds remaining";
 
 
-var playerScore = 0;
-
-var submitBtn = document.querySelector("#submit");
-  submitBtn.addEventListener('click', function() {
-    event.preventDefault();
-    awardPoints();
-  });
-
-  const radioButtons = document.querySelectorAll('input[name="answer"]');
-
-  // Create a variable to store the player's selected answer
-  let selectedAnswer;
-  
-  // Loop over the radio buttons and add a change event listener to each one
-  for (let i = 0; i < radioButtons.length; i++) {
-    radioButtons[i].addEventListener('change', function() {
-      selectedAnswer = this.value;
+function nextQuestion() {
+  console.log(questions[index]);
+  var question = questions[index];
+  var choices = question.choices;
+  var questionText = document.querySelector("#question");
+  questionText.textContent = question.question;
+  var choicesList = document.querySelector("#choices");
+  choicesList.innerHTML = "";
+  for (var i = 0; i < choices.length; i++) {
+    var choice = choices[i];
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.setAttribute("class", "choice");
+    choiceButton.setAttribute("value", choice);
+    choiceButton.addEventListener("click", function (event) {
+      var answer = event.target.value;
+      if (answer === questions[index].answer) {
+        score++;
+        document.getElementById('playerScore').textContent = score;
+      }
+      index++;
+      if (index === questions.length) {
+        endGame();
+      } else {
+        nextQuestion();
+      }
     });
+    choicesList.appendChild(choiceButton);
   }
-  
-  // Function to award the player 10 points for the correct answer
-  function awardPoints() {
+}
 
-    var tooltip = document.querySelector("#tooltip");
-    var scoreDisplay = document.querySelector("#scoreDisplay");
 
-    if (quizAnswers.includes(selectedAnswer)) {
-      tooltip.innerHTML = "Correct!";
-        playerScore += 10;
-        scoreDisplay.innerHTML = playerScore;
-    } else {
-      tooltip.innerHTML = "Incorrect!";
-        playerScore -= 5;
-        scoreDisplay.innerHTML = playerScore;
-    }
-  }
-
-  var submitBtn = document.querySelector("#submit");
-  submitBtn.addEventListener('click', function() {
-    event.preventDefault();
-    nextQuestion();
-  });
-
-    // Function to move to the next question
-
-    function nextQuestion() {
-        var labels = document.querySelectorAll('label');
-
-        labels[0].innerHTML = 'Netscape';
-        labels[1].innerHTML = 'Internet Explorer';
-        labels[2].innerHTML = 'Mosaic';
-        labels[3].innerHTML = 'Firefox';
-
-        var tooltip = document.querySelector("#tooltip");
-        var scoreDisplay = document.querySelector("#scoreDisplay");
-    
-        if (quizAnswers.includes(selectedAnswer)) {
-          tooltip.innerHTML = "Correct!";
-            playerScore += 10;
-            scoreDisplay.innerHTML = playerScore;
-        } else {
-          tooltip.innerHTML = "Incorrect!";
-            playerScore -= 5;
-            scoreDisplay.innerHTML = playerScore;
-        }
-    }
-
-    var submitBtn = document.querySelector("#submit");
-  submitBtn.addEventListener('click', function() {
-    event.preventDefault();
-    nextQuestion2();
-  });
-
-    // Function to move to the next question
-
-    function nextQuestion2() {
-        var labels = document.querySelectorAll('label');
-
-        labels[0].innerHTML = 'Opens the console';
-        labels[1].innerHTML = 'Opens the window';
-        labels[2].innerHTML = 'Opens the browser';
-        labels[3].innerHTML = 'Opens the document';
-
-        var tooltip = document.querySelector("#tooltip");
-        var scoreDisplay = document.querySelector("#scoreDisplay");
-    
-        if (quizAnswers.includes(selectedAnswer)) {
-          tooltip.innerHTML = "Correct!";
-            playerScore += 10;
-            scoreDisplay.innerHTML = playerScore;
-        } else {
-          tooltip.innerHTML = "Incorrect!";
-            playerScore -= 5;
-            scoreDisplay.innerHTML = playerScore;
-        }
-    }
-
-    var submitBtn = document.querySelector("#submit");
-  submitBtn.addEventListener('click', function() {
-    event.preventDefault();
-    nextQuestion3();
-  });
-
-    // Function to move to the next question
-
-    function nextQuestion3() {
-        var labels = document.querySelectorAll('label');
-
-        labels[0].innerHTML = 'function myFunction()';
-        labels[1].innerHTML = 'function myFunction';
-        labels[2].innerHTML = 'myFunction()';
-        labels[3].innerHTML = 'myFunction';
-
-        var tooltip = document.querySelector("#tooltip");
-        var scoreDisplay = document.querySelector("#scoreDisplay");
-    
-        if (quizAnswers.includes(selectedAnswer)) {
-          tooltip.innerHTML = "Correct!";
-            playerScore += 10;
-            scoreDisplay.innerHTML = playerScore;
-        } else {
-          tooltip.innerHTML = "Incorrect!";
-            playerScore -= 5;
-            scoreDisplay.innerHTML = playerScore;
-        }
-    }
-
-    var submitBtn = document.querySelector("#submit");
-    submitBtn.addEventListener('click', function() {
-      event.preventDefault();
-      nextQuestion4();
-    });
-
-    // Function to move to the next question
-
-    function nextQuestion4() {
-
-        var labels = document.querySelectorAll('label');
-
-        labels[0].innerHTML = '1995';
-        labels[1].innerHTML = '1996';
-        labels[2].innerHTML = '1997';
-        labels[3].innerHTML = '1998';
-
-        var tooltip = document.querySelector("#tooltip");
-        var scoreDisplay = document.querySelector("#scoreDisplay");
-    
-        if (quizAnswers.includes(selectedAnswer)) {
-          tooltip.innerHTML = "Correct!";
-            playerScore += 10;
-            scoreDisplay.innerHTML = playerScore;
-        } else {
-          tooltip.innerHTML = "Incorrect!";
-            playerScore -= 5;
-            scoreDisplay.innerHTML = playerScore;
-        }
-    }
-
+function endGame(event) {
+  console.log("Game over!");
+  var initials = window.prompt("Enter your initials to save your score.");
+  window.alert(initials + "! Your score is " + score + " out of " + questions.length + ".");
+  document.getElementById("previousScores").innerHTML = initials + " - " + score + " out of " + questions.length;
+};
